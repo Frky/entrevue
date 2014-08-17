75c3bd746122b8a96f23f546733452d802746899
Bl3f <christophe.blefari@gmail.com>
2014-08-17 16:25:47 +0200

    Activate admin

--- rdv/admin.py
+++ rdv/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from rdv.models import RDV, Answer
+
+
+class RDVAdmin(admin.ModelAdmin):
+    pass
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(RDV, RDVAdmin)
+admin.site.register(Answer, AnswerAdmin)
+
